7abcf8affcd590927d69c36297c6e1488517398c
ProCode <68168458+ProCode-Software@users.noreply.github.com>
2023-03-29 16:38:45 -0500

    Add narrator setting to BiblePuzzle (Beta)

--- js/main.js
+++ js/main.js
@@ -1,96 +1,3 @@
-const randomVerses = [
-    {
-        verse: 'For all have sinned and fall short of the glory of God, and all are justified freely by his grace through the redemption that came by Christ Jesus.',
-        ref: 'Romans 3:23-24',
-        imageURL: 'https://pbs.twimg.com/media/EVfFFFxX0Ag8QAO.jpg',
-        source: 'The Bible on Twitter'
-    },
-    {
-        verse: 'If you declare with your mouth, "Jesus is Lord," and believe in your heart that God raised him from the dead, you will be saved. For it is with your heart that you believe and are justified, and it is with your mouth that you profess your faith and are saved.',
-        ref: 'Romans 10:9-10',
-        imageURL: 'https://d347bo4ltvvnaz.cloudfront.net/images/ScriptureArt_0117_-_Romans_10_9_157x157.jpg',
-        source: 'Faith Chapel Assembly of God Pleasanton'
-    },
-    {
-        verse: 'Whoever believes in the Son has eternal life, but whoever rejects the Son will not see life, for God\'s wrath remains on them.',
-        ref: 'John 3:36',
-        imageURL: 'https://pbs.twimg.com/media/Eo3AFdhXIAE7RBU.jpg:large',
-        source: 'James Lamont Newson on Twitter'
-    },
-    {
-        verse: 'If we confess our sins, he is faithful and just and will forgive us our sins and purify us from all unrighteousness.',
-        ref: '1 John 1:9',
-        imageURL: 'https://idisciple.blob.core.windows.net/idm/1john1_9_promo_en-1-OriginalWithCut-774x1376-90-CardBanner.Jpeg',
-        source: 'iDisciple'
-    },
-    {
-        verse: 'For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life. For God did not send his Son into the world to condemn the world, but to save the world through him.',
-        ref: 'John 3:16-17',
-        imageURL: 'https://imageproxy.youversionapi.com/1280x1280/https://s3.amazonaws.com/static-youversionapi-com/images/base/81638/1280x1280.jpg',
-        source: 'YouVersion'
-    },
-    {
-        verse: 'For the wages of sin is death, but the gift of God is eternal life in Christ Jesus our Lord.',
-        ref: 'Romans 6:23',
-        imageURL: 'https://imageproxy.youversionapi.com/320x320/https://s3.amazonaws.com/static-youversionapi-com/images/base/62584/1280x1280.jpg',
-        source: 'YouVersion'
-    },
-    {
-        verse: 'For it is by grace you have been saved, through faith-and this is not from yourselves, it is the gift of God-not by works, so that no one can boast.',
-        ref: 'Ephesians 2:8-9',
-        imageURL: 'https://media.swncdn.com/cms/IB/55592-eph2-8-9.1200w.tn.jpg',
-        source: 'Bible Study Tools'
-    },
-    {
-        verse: 'He is the atoning sacrifice for our sins, and not only for ours but also for the sins of the whole world.',
-        ref: '1 John 2:2',
-        imageURL: 'https://ymi.today/wp-content/uploads/2018/03/03_oct_bv.jpg',
-        source: 'YMI'
-    },
-    {
-        verse: 'But he was pierced for our transgressions, he was crushed for our iniquities; the punishment that brought us peace was on him, and by his wounds we are healed.',
-        ref: 'Isaiah 53:5',
-        imageURL: 'https://votd.olivetree.com/03_31_NIV.jpg',
-        source: 'OliveTree'
-    },
-    {
-        verse: 'But God demonstrates his own love for us in this: While we were still sinners, Christ died for us.',
-        ref: 'Romans 5:8',
-        imageURL: 'https://i1.wp.com/www.busyandlovingit.com/wp-content/uploads/2016/03/Romans-5_8.jpg',
-        source: 'Busy and Loving It!'
-    },
-    {
-        verse: 'In reply Jesus declared, "I tell you the truth, no one can see the kingdom of God unless he is born again."',
-        ref: 'John 3:3',
-        imageURL: 'https://biblia.com/bible/images/640x480/Jn3.3?extension=png&fallbackOnFailure=false',
-        source: 'Faithlife'
-    },
-    {
-        verse: 'And the God of all grace, who called you to his eternal glory in Christ, after you have suffered a little while, will himself restore you and make you strong, firm and steadfast.',
-        ref: '1 Peter 5:10',
-        imageURL: 'https://imageproxy.youversionapi.com/320x320/https://s3.amazonaws.com/static-youversionapi-com/images/base/68496/1280x1280.jpg',
-        source: 'YouVersion'
-    },
-    {
-        verse: 'If we claim to have fellowship with him and yet walk in the darkness, we lie and do not live out the truth. But if we walk in the light, as he is in the light, we have fellowship with one another, and the blood of Jesus, his Son, purifies us from all sin.',
-        ref: '1 John 1:6-7',
-        imageURL: 'https://imageproxy.youversionapi.com/640x640/https://s3-us-west-2.amazonaws.com/mushroom-verse-images-prod/180536297-1679020452821.jpg',
-        source: 'ProCode Software'
-    },
-    {
-        verse: 'In him we have redemption through his blood, the forgiveness of sins, in accordance with the riches of God’s grace that he lavished on us. With all wisdom and understanding,',
-        ref: 'Ephesians 1:7-8',
-        imageURL: 'https://imageproxy.youversionapi.com/640x640/https://s3-us-west-2.amazonaws.com/mushroom-verse-images-prod/180536297-1679276686270.jpg',
-        source: 'ProCode Software'
-    },
-    {
-        verse: 'Consequently, you are no longer foreigners and strangers, but fellow citizens with God’s people and also members of his household, built on the foundation of the apostles and prophets, with Christ Jesus himself as the chief cornerstone.',
-        ref: 'Ephesians 2:19-20',
-        imageURL: 'https://imageproxy.youversionapi.com/640x640/https://s3-us-west-2.amazonaws.com/mushroom-verse-images-prod/180536297-1679276615769.jpg',
-        source: 'ProCode Software'
-    }
-]
-
 const systemIcons = {
     "bookmark": `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M4.85388 5.75V20.75L12 15.1253L19.1461 20.75V5.75C19.1461 4.36929 18.0268 3.25 16.6461 3.25H7.35388C5.97317 3.25 4.85388 4.36929 4.85388 5.75Z" stroke="currentColor" stroke-width="1.75" stroke-linejoin="round"/>
@@ -154,7 +61,8 @@ let settingsValues = {
     darkMode: false,
     backSpacing: true,
     mobileKeyboard: false,
-    themeColor: 4
+    themeColor: 4,
+    narrator: false,
 }
 let stats = {
     bestVerse: 1, // Based on score.
@@ -697,6 +605,13 @@ const settingsModel = [
                 value: "mobileKeyboard",
 
                 description: 'Show touch keyboard button. Best for mobile users.'
+            },
+            {
+                type: 'toggle',
+                title: 'Narrator <div class="textTag beta" title="This feature is experimental and is still receiving updates and new features that may be unfinished.">Beta</div>',
+                value: "narrator",
+
+                description: 'Give spoken feedback while typing.'
             },
         ]
     },
