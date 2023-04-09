4e852f7ff52a04686682d448895e7e6461810231
ProCode <68168458+ProCode-Software@users.noreply.github.com>
2023-04-09 18:13:57 -0500

    Edit

---
+++ js/dev.js
@@ -0,0 +1,20 @@
+const developerCommands = [
+    {
+        cmd: 'disabledevmode',
+        desc: 'Disables developer mode'
+    },
+    {
+        cmd: 'showverses',
+        desc: 'Shows all BiblePuzzle verses'
+    },
+    {
+        cmd: 'setincorrectchars',
+        desc: 'Simulate incorrect typed characters',
+        params: ['incorrectChars:number']
+    },
+    {
+        cmd: 'togglecpsview',
+        desc: 'Toggles the characters/sec counter'
+    },
+]
+const developerViews = []
